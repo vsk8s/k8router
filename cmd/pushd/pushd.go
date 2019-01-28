1ab696fb24389e36b694a6ecd1b1e2f76148e4ea
Maximilian Falkenstein <maximilian.falkenstein@sos.ethz.ch>
2019-01-28 14:04:04 +0100

    Initial import

---
+++ cmd/pushd/pushd.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+func main() {
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Get all pods (example)
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+}
---
+++ pkg/proto/ingress_gen.go
@@ -0,0 +1,3 @@
+//go:generate protoc --go_out=import_prefix=github.com/soseth/k8router/pkg/proto:. ingress.proto
+
+package proto
