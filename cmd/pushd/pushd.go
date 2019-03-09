46477631d6e9b518a50b12ffec9e4e9b49b92af4
Jan Veen <veenj@vis.ethz.ch>
2019-03-09 15:38:09 +0100

    Extend pushd
    Collect ingresses from all namespaces

--- cmd/pushd/pushd.go
+++ cmd/pushd/pushd.go
@@ -5,11 +5,17 @@ import (
     "time"
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     "k8s.io/client-go/kubernetes/typed/extensions/v1beta1"
+    "k8s.io/client-go/kubernetes"
     "k8s.io/client-go/rest"
 )
 
 func main() {
     config, err := rest.InClusterConfig()
+    if err != nil {
+        panic(err.Error())
+    }
+
+    coreclient, err := kubernetes.NewForConfig(config)
     if err != nil {
         panic(err.Error())
     }
@@ -20,13 +26,25 @@ func main() {
     }
 
     for {
-        items, err := client.Ingresses("dev").List(metav1.ListOptions{})
 
+        namespaces, err := coreclient.CoreV1().Namespaces().List(metav1.ListOptions{})
         if err != nil {
-            fmt.Printf(err.Error())
-            fmt.Printf("\n")
-        } else {
-            fmt.Printf("There are %d ingresses in the cluster\n", len(items.Items))
+            fmt.Printf("%s\n", err.Error())
+        }
+
+        for i := 0; i < len(namespaces.Items); i++ {
+
+            nsName := namespaces.Items[i].ObjectMeta.Name
+
+            items, err := client.Ingresses(nsName).List(metav1.ListOptions{})
+
+            if err != nil {
+                fmt.Printf("%s\n", err.Error())
+            } else {
+                fmt.Printf("There are %d ingresses in namespace '%s'\n",
+                    len(items.Items),
+                    nsName)
+            }
         }
 
         time.Sleep(1000 * time.Millisecond)
