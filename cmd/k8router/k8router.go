90ed3f4713a34a0c6a3bde77cdaa54c8d13cd931
Ben Fiedler <bfiedler@ethz.ch>
2019-03-24 15:55:49 +0100

    Use proper logging framework

--- cmd/k8router/k8router.go
+++ cmd/k8router/k8router.go
@@ -5,17 +5,16 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"github.com/imdario/mergo"
 	"github.com/jessevdk/go-flags"
+	"github.com/op/go-logging"
 	"gopkg.in/yaml.v2"
 	v1beta1api "k8s.io/api/extensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes/typed/extensions/v1beta1"
 	"k8s.io/client-go/tools/clientcmd"
-	"log"
 	"net"
 	"os"
 	"strings"
@@ -28,6 +27,8 @@ var (
 
 	frontendTemplate = template.Must(template.New("haproxy-frontend").Parse(haproxyFrontendTemplate))
 	backendTemplate  = template.Must(template.New("haproxy-backend").Parse(haproxyBackendTemplate))
+
+	logger = logging.MustGetLogger("k8router")
 
 	opts          CommandlineFlags
 	config        Config
@@ -66,40 +67,46 @@ type Change = struct {
 
 type CommandlineFlags = struct {
 	ConfigFile string `short:"c" long:"config" description:"Configuration file to use" default:"/etc/k8router/config.yml"`
-	Version    bool   `long:"version" description:"Output version info and exit"`
+
+	Debug   bool `short:"d" long:"debug" description:"Turn on debug output"`
+	Verbose bool `short:"v" long:"verbose" description:"Turn on verbose output"`
+	Silent  bool `short:"s" long:"silent" description:"Only log warnings and errors"`
+
+	Version bool `long:"version" description:"Output version info and exit"`
 }
 
 func main() {
-	log.Printf("Starting k8router %s", version)
-	log.Printf("(C) 2019 The Kubernauts")
-
 	_, err := flags.Parse(&opts)
 	if err != nil {
-		return
+		logger.Fatalf("Error parsing flags")
 	}
+	setupLogger()
+
+	logger.Noticef("Starting k8router %s", version)
+	logger.Notice("(C) 2019 The Kubernauts")
 
 	if opts.Version {
 		return
 	}
 
-	rc := parseK8RouterConfig(opts.ConfigFile)
 	backendIPs = make(map[string][]string)
 	ingresses = make(map[string]map[string][]string)
 	changes := make(chan Change)
+	rc := parseK8RouterConfig(opts.ConfigFile)
 
-	log.Print("Read config and templates, connecting to clusters")
+	logger.Info("Read config and templates, connecting to clusters")
 
 	for _, cluster := range rc.Clusters {
 		ingressWatcher, err := cluster.Client.Ingresses("").Watch(metav1.ListOptions{})
 		if err != nil {
 			// do something gracefully here
-			panic(err.Error())
+			logger.Fatalf("Could not watch ingresses: %s", err.Error())
 		}
 
 		go watchIngresses(ingressWatcher, cluster, changes)
 	}
 
-	log.Printf("Connected to the clusters, now applying changes")
+	logger.Info("Connected to the clusters, now applying changes")
 
 	apply(changes)
 }
@@ -108,10 +115,10 @@ func apply(changes <-chan Change) {
 	for change := range changes {
 		ingress, ok := change.Event.Object.(*v1beta1api.Ingress)
 		if !ok {
-			panic("not an ingress")
+			logger.Fatalf("Given object wasn't an ingress!")
 		}
 
-		log.Printf("%s %s %s", change.Event.Type, change.Cluster.Name, ingress.Name)
+		logger.Noticef("%s %s %s", change.Event.Type, change.Cluster.Name, ingress.Name)
 		changedHosts := make([]string, 0)
 
 		// Get service IPs of the ingresses (we skip this in v1 of the proxy)
@@ -122,11 +129,13 @@ func apply(changes <-chan Change) {
 				backendIPs[rule.Host] = append(backendIPs[rule.Host], change.Cluster.IPs...)
 				ingresses[change.Cluster.Name][ingress.Name] = append(ingresses[change.Cluster.Name][ingress.Name], rule.Host)
 				changedHosts = append(changedHosts, rule.Host)
+				logger.Infof("Added IPs for cluster %s for host %s from ingress %s to maps", change.Cluster.Name, rule.Host, ingress.Name)
 			}
 		case watch.Deleted:
 			for _, rule := range ingress.Spec.Rules {
 				backendIPs[rule.Host] = sliceDifference(backendIPs[rule.Host], change.Cluster.IPs)
 				changedHosts = append(changedHosts, rule.Host)
+				logger.Infof("Deleted IPs for cluster %s and host %s from ingress %s", change.Cluster.Name, rule.Host, ingress.Name)
 			}
 			delete(ingresses[change.Cluster.Name], ingress.Name)
 		case watch.Modified:
@@ -136,6 +145,7 @@ func apply(changes <-chan Change) {
 				changedHosts = hosts
 				for _, host := range ingresses[change.Cluster.Name][ingress.Name] {
 					backendIPs[host] = sliceDifference(backendIPs[host], change.Cluster.IPs)
+					logger.Infof("Deleted IPs for cluster %s and host %s from ingress %s", change.Cluster.Name, host, ingress.Name)
 				}
 				delete(ingresses[change.Cluster.Name], ingress.Name)
 			}
@@ -144,9 +154,10 @@ func apply(changes <-chan Change) {
 				backendIPs[rule.Host] = append(backendIPs[rule.Host], change.Cluster.IPs...)
 				ingresses[change.Cluster.Name][ingress.Name] = append(ingresses[change.Cluster.Name][ingress.Name], rule.Host)
 				changedHosts = append(changedHosts, rule.Host)
+				logger.Infof("Added IPs for cluster %s and host %s from ingress %s", change.Cluster.Name, rule.Host, ingress.Name)
 			}
 		case watch.Error:
-			log.Printf("Error watching ingresses in cluster %s", change.Cluster.Name)
+			logger.Fatalf("Error watching ingresses in cluster %s", change.Cluster.Name)
 			return
 		default:
 			return
@@ -155,14 +166,14 @@ func apply(changes <-chan Change) {
 		// template the corresponding ingress config
 		for _, host := range changedHosts {
 			updateConfig(host)
-			//log.Printf("%+v", ingresses)
-			//log.Printf("%+v", backendIPs)
+			logger.Debugf("ingresses: %+v", ingresses)
+			logger.Debugf("backendIPs: %+v", backendIPs)
 		}
 	}
 }
 
 func updateConfig(host string) {
-	log.Printf("  %s", host)
+	logger.Debugf("Writing config for host %s", host)
 	printedHost := strings.Replace(host, ".", "-", -1)
 
 	ips, ok := backendIPs[host]
@@ -174,20 +185,21 @@ func updateConfig(host string) {
 	if len(ips) == 0 {
 		err := os.Remove(fmt.Sprintf(config.HAProxyConfigDir+"71-%s.conf", host))
 		if err != nil {
-			panic(err)
+			logger.Fatal(err.Error())
 		}
 		err = os.Remove(fmt.Sprintf(config.HAProxyConfigDir+"75-%s.conf", host))
 		if err != nil {
-			panic(err)
+			logger.Fatal(err.Error())
 		}
-		// Now it is safe to delete this ingress from the dictionary
+		// Now it is safe to delete this host from the dictionary
 		delete(backendIPs, host)
+		logger.Infof("Deleted host %s from the backendIPs dictionary", host)
 		return
 	}
 
 	frontendConfig, err := os.Create(fmt.Sprintf(config.HAProxyConfigDir+"71-%s.conf", host))
 	if err != nil {
-		panic(err.Error())
+		logger.Fatal(err.Error())
 	}
 
 	err = frontendTemplate.Execute(frontendConfig, struct {
@@ -195,19 +207,19 @@ func updateConfig(host string) {
 		ActualHost string
 	}{printedHost, host})
 	if err != nil {
-		panic(err.Error())
+		logger.Fatal(err.Error())
 	}
 
 	backendConfig, err := os.Create(fmt.Sprintf(config.HAProxyConfigDir+"75-%s.conf", host))
 	if err != nil {
-		panic(err.Error())
+		logger.Fatal(err.Error())
 	}
 	err = backendTemplate.Execute(backendConfig, struct {
 		Host string
 		IPs  []string
 	}{printedHost, backendIPs[host]})
 	if err != nil {
-		panic(err.Error())
+		logger.Fatal(err.Error())
 	}
 
 	// todo: restart haproxy
@@ -223,18 +235,18 @@ func parseK8RouterConfig(path string) RuntimeConfig {
 	runtimeConfig := RuntimeConfig{}
 	content, err := os.Open(path)
 	if err != nil {
-		panic(err.Error())
+		logger.Fatal(err.Error())
 	}
 
 	decoder := yaml.NewDecoder(content)
 	err = decoder.Decode(&config)
 	if err != nil {
-		panic(err.Error())
+		logger.Fatal(err.Error())
 	}
 
 	err = mergo.Merge(&config, defaultConfig)
 	if err != nil {
-		panic(err.Error())
+		logger.Fatal(err.Error())
 	}
 
 	if !strings.HasSuffix(config.HAProxyConfigDir, "/") {
@@ -243,28 +255,28 @@ func parseK8RouterConfig(path string) RuntimeConfig {
 
 	for _, clusterConfig := range config.ClusterConfigs {
 		if clusterConfig.ClusterName == "" {
-			panic("cluster name may not be empty")
+			logger.Fatal("Cluster name may not be empty")
 		}
 
 		for _, ip := range clusterConfig.IngressServiceIPs {
 			if res := net.ParseIP(ip); res == nil {
-				panic(errors.New(fmt.Sprintf("invalid IP: %s", ip)))
+				logger.Fatalf("Invalid IP in cluster %s: %s", clusterConfig.ClusterName, ip)
 			}
 		}
 
 		kubeConfig, err := clientcmd.LoadFromFile(clusterConfig.ConfigPath)
 		if err != nil {
-			panic(err.Error())
+			logger.Fatal(err.Error())
 		}
 
 		restConfig, err := clientcmd.NewDefaultClientConfig(*kubeConfig, &clientcmd.ConfigOverrides{}).ClientConfig()
 		if err != nil {
-			panic(err.Error())
+			logger.Fatal(err.Error())
 		}
 
 		client, err := v1beta1.NewForConfig(restConfig)
 		if err != nil {
-			panic(err.Error())
+			logger.Fatal(err.Error())
 		}
 
 		ingresses[clusterConfig.ClusterName] = make(map[string][]string)
@@ -300,6 +312,24 @@ func sliceDifference(as, bs []string) []string {
 		}
 	}
 	return res
+}
+
+func setupLogger() {
+	format := logging.MustStringFormatter(
+		`[%{level:.8s}] (%{shortfunc}) - %{message}`,
+	)
+	backend := logging.NewBackendFormatter(logging.NewLogBackend(os.Stderr, "", 0), format)
+	leveledBackend := logging.AddModuleLevel(backend)
+	if opts.Debug {
+		leveledBackend.SetLevel(logging.DEBUG, "")
+	} else if opts.Verbose {
+		leveledBackend.SetLevel(logging.INFO, "")
+	} else if opts.Silent {
+		leveledBackend.SetLevel(logging.WARNING, "")
+	} else {
+		leveledBackend.SetLevel(logging.NOTICE, "")
+	}
+	logging.SetBackend(leveledBackend)
 }
 
 const haproxyFrontendTemplate = `    acl {{ .Host }} req_ssl_sni -i {{ .ActualHost }}
