6de050ab6e38c7af471b03bc33527bba9766e3ba
Maximilian Falkenstein <maximilian.falkenstein@sos.ethz.ch>
2019-05-04 15:27:38 +0200

    Config handler: Improve testing

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -9,24 +9,35 @@ import (
 	"testing"
 )
 
+// Helper function to write a config string to file and load it
+func writeAndLoadConfig(config string, t *testing.T) (*Config, error) {
+	dir := os.TempDir()
+	file := path.Join(dir, "testfile")
+	err := ioutil.WriteFile(file, []byte(config), 0644)
+	if err != nil {
+		t.Error(err)
+		return nil, err
+	}
+
+	return FromFile(file)
+}
+
+// Helper function to check whether loading the specified config produces the specified error
+func testError(config string, error string, t *testing.T, g *gomega.WithT) {
+	_, err := writeAndLoadConfig(config, t)
+	g.Expect(err).NotTo(gomega.BeNil(), "This should have resulted in an error")
+	g.Expect(err.Error()).To(gomega.BeIdenticalTo(error), "This should have matched the error description")
+}
+
+// Check a basic invalid config parse
 func TestInvalidConfigParse(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
 	configStr := `
 haproxyTemplatePath: /foo/bar/test.cfg
 clusters:
   - name: testcluster
 `
-	dir := os.TempDir()
-	file := path.Join(dir, "testfile")
-	err := ioutil.WriteFile(file, []byte(configStr), 0644)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	_, err = FromFile(file)
-	if err == nil || err.Error() != "Cluster: kubeconfig missing" {
-		t.Fail()
-	}
+	testError(configStr, "Cluster: kubeconfig missing", t, g)
 }
 
 func TestDefaultConfigParse(t *testing.T) {
@@ -39,20 +50,13 @@ clusters:
     kubeconfig: /etc/kubernetes/kubeconfig.yml
 certificates:
   - cert: /foo
+    name: foo
     domains:
       - example.org
 ips:
   - 127.0.0.1
 `
-	dir := os.TempDir()
-	file := path.Join(dir, "testfile")
-	err := ioutil.WriteFile(file, []byte(configStr), 0644)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	uut, err := FromFile(file)
+	uut, err := writeAndLoadConfig(configStr, t)
 	if err != nil {
 		t.Error(err)
 		return
@@ -65,4 +69,79 @@ ips:
 	g.Expect(uut.Clusters[0].IngressPort).To(gomega.BeIdenticalTo(80))
 	g.Expect(len(uut.IPs)).To(gomega.BeIdenticalTo(1))
 	g.Expect(*uut.IPs[0]).To(gomega.BeEquivalentTo(net.ParseIP("127.0.0.1")))
+}
+
+func TestErrorConditions(t *testing.T) {
+	// Cluster config issues
+	g := gomega.NewGomegaWithT(t)
+	configStr := `
+haproxyTemplatePath: /foo/bar/test.cfg
+clusters:
+  - kubeconfig: /foo/bar
+`
+	testError(configStr, "Cluster: name missing", t, g)
+
+	// Certificate config issues
+	configStr = `
+haproxyTemplatePath: /foo/bar/test.cfg
+clusters:
+  - kubeconfig: /foo/bar
+    name: foo
+certificates:
+  - name: foo
+    domains:
+      - example.org
+`
+	testError(configStr, "Certificate: cert missing", t, g)
+	configStr = `
+haproxyTemplatePath: /foo/bar/test.cfg
+clusters:
+  - kubeconfig: /foo/bar
+    name: foo
+certificates:
+  - cert: /foo
+    domains:
+      - example.org
+`
+	testError(configStr, "Certificate: name missing", t, g)
+	configStr = `
+haproxyTemplatePath: /foo/bar/test.cfg
+clusters:
+  - kubeconfig: /foo/bar
+    name: foo
+certificates:
+  - cert: /foo
+    name: foo
+`
+	testError(configStr, "Certificate: cert is not valid for any domain?", t, g)
+
+	// overall config issues
+	configStr = `
+haproxyTemplatePath: /foo/bar/test.cfg
+clusters:
+  - kubeconfig: /foo/bar
+    name: foo
+`
+	testError(configStr, "Certificate list missing", t, g)
+	configStr = `
+haproxyTemplatePath: /foo/bar/test.cfg
+certificates:
+  - cert: /foo
+    name: foo
+    domains:
+      - example.org
+`
+	testError(configStr, "Cluster list missing", t, g)
+	configStr = `
+haproxyTemplatePath: /foo/bar/test.cfg
+certificates:
+  - cert: /foo
+    name: foo
+    domains:
+      - example.org
+clusters:
+  - kubeconfig: /foo/bar
+    name: foo
+`
+	testError(configStr, "IP list missing", t, g)
 }
